e449693b7bf1807b38bef4d07a6181f916dc60a9
eunbee <4leaf.PEB>
2021-04-21 19:26:59 +0900

    fokin-weather

--- App.js
+++ App.js
@@ -2,23 +2,49 @@ import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import { Alert } from "react-native";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "424b38a68240d94c68f68d4457a84db0";
 
 export default class extends React.Component {
+  state = { isLoading: true };
+
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({ isLoading: false, temp, condition: weather[0].main });
+  };
   // 사용자의 위치를 얻음
+
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
-      console.log(reponse);
-      const location = await Location.getCurrentPositionAsync();
-      console.log(location);
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
   };
+
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
+// Math.round => 소수점 위로 올리기
